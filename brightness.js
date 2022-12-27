7bb15b26eb5b58fa786a3b259201982d71b91e2b
Darren Kameoka <>
2022-12-27 03:37:46 -1000

    refactor: less nesting

--- brightness.js
+++ brightness.js
@@ -2,41 +2,47 @@
 let brightness_input = document.getElementById('brightness');
 
 // TODO: Switch to "browser.storage.session"; though, it does not yet exist in Firefox as of Dec 2022.
+async function set_brightness(tabId,value) {
+    // Set new brightness first to reduce flashing.
+    let brightness_step = parseInt(value);
+    await browser.scripting.insertCSS({
+        target: {tabId: tabId},
+        css: 'html {filter: brightness(' + brightness_step * 10 + '%) !important;}'
+    });
+
+    // Iterate to remove all other possible inserted CSS.
+    for (let step = 0;step < 10;step++) {
+        if (step == brightness_step) {
+            continue;
+        }
+        await browser.scripting.removeCSS({
+            target: {tabId: tabId},
+            css: 'html {filter: brightness(' + step * 10 + '%) !important;}'
+        });
+    }
+
+    // Store new brightness
+    await browser.sessions.setTabValue(tabId,'brightness_step',brightness_step);
+};
+
 brightness_input.oninput = function(e) {
-    browser.tabs.query({active: true, currentWindow: true}).then(async tabs => {
+    browser.tabs.query({active: true,currentWindow: true}).then(async tabs => {
         for (const tab of tabs) {
-            // Set new brightness first to reduce flashing.
-            let brightness_step = parseInt(e.target.value);
-            await browser.scripting.insertCSS({
-                target: {tabId: tab.id},
-                css: 'html {filter: brightness(' + brightness_step * 10 + '%) !important;}'
-            });
-
-            // Iterate to remove all other possible inserted CSS.
-            for (let step = 0;step < 10;step++) {
-                if (step == brightness_step) {
-                    continue;
-                }
-                browser.scripting.removeCSS({
-                    target: {tabId: tab.id},
-                    css: 'html {filter: brightness(' + step * 10 + '%) !important;}'
-                });
-            }
-
-            // Store new brightness
-            browser.sessions.setTabValue(tab.id,'brightness_step',brightness_step);
+            set_brightness(tab.id,e.target.value);
         }
     });
 };
 
-browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
+browser.tabs.query({active: true,currentWindow: true}).then(tabs => {
     for (const tab of tabs) {
         browser.sessions.getTabValue(tab.id,'brightness_step').then(result => {
             if (result == undefined) {
                 brightness_input.value = '100';
             } else {
                 brightness_input.value = result;
+                set_brightness(tab.id,result);
             }
         });
     }
 });
+
